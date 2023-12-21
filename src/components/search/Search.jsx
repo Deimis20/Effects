132a273fa2ae53f7b0b17defb8b0eaa1aa4d720c
Moksleivis <d.gricius12@gmail.com>
2023-12-21 12:55:30 +0200

    mod

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,30 +1,25 @@
 import { useEffect, useState } from "react";
 
 const Search = () => {
-  const [breeds, setBreeds] = useState([]);
-  const [setInputValue] = useState("");
-  useEffect(() => {
-    fetch("https://api.thecatapi.com/v1/breeds").then((response) =>
-      response.json().then((data) => setBreeds(data))
-    );
-  }, []);
-  const handleSearch = (e) => {
-    setInputValue(e.targe.value);
-  };
-  return (
-    <>
-      <input
-        id="breedInput"
-        list="catBreedList"
-        placeholder="Iveskite kates veisle.."
-        onChange={setInputValue}
-      />
-      <datalist id="catBreedList">
-        {breeds?.map((breeds) => (
-          <option key={breeds.id} value={breeds.name}></option>
-        ))}
-      </datalist>
-    </>
-  );
-};
+    const [breeds, setBreeds] = useState([]);
+  const [InputValue, setInputValue] = useState('')
+    useEffect(() => {
+          fetch("https://api.thecatapi.com/v1/breeds")
+            .then(response => response.json()
+            .then(data => setBreeds(data)));
+    }, []);
+    const handleSearch=(e)=>{
+      props.onSearch(e.target.value)
+    };
+    return(
+<>
+<input id="breedInput" list="catBreedList" placeholder="Iveskite kates veisle.." onChange={setInputValue} />
+<datalist id="catBreedList">
+{breeds?.map((breeds)=>
+<option key={breeds.id} value={breeds.name}></option>)}
+</datalist>
+
+</>
+
+    )}
 export default Search;
